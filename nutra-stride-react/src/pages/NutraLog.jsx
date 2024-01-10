4d82582c988e345ee58e3042e2b4b2e681cf9f24
Connor <c.meriweather55@gmail.com>
2024-01-10 16:53:01 -0600

    amended NutraLog, fixed error w axios, added styles

--- nutra-stride-react/src/pages/NutraLog.jsx
+++ nutra-stride-react/src/pages/NutraLog.jsx
@@ -8,26 +8,19 @@ const NutraLog = () => {
   const [loggedItems, setLoggedItems] = useState([]);
 
   const logFood = async (e) => {
-
     e.preventDefault();
 
+    // Your API credentials and URL
     const appId = "8dd733fb";
     const appKey = "439705ccac3ff7fb3c5efbeee90d7e4f";
-
     const apiUrl = `https://api.edamam.com/api/nutrition-data?app_id=${appId}&app_key=${appKey}`;
 
     try {
-      const response = await axios.post(apiUrl, {
-        params: {
-        ingr: [foodItem], // An array of food items to analyze
-        },
-      });
+      // Make a GET request using Axios
+      const response = await axios.get(`${apiUrl}&ingr=${foodItem}`);
+      const data = response.data;
 
       if (response.status === 200) {
-        const data = response.data;
-
-        console.log("Data from API:", data);
-
         // Update the list of logged items
         setLoggedItems((prevItems) => [
           ...prevItems,
@@ -38,8 +31,8 @@ const NutraLog = () => {
         setCalories(data.calories);
         console.log(`Logged food: ${foodItem}, calories: ${data.calories}`);
       } else {
-        setLogError(response.data.message);
-        console.error("Food logging failed:", response.data.message);
+        setLogError(data.message);
+        console.error("Food logging failed:", data.message);
       }
     } catch (error) {
       setLogError("An unexpected error occurred during food logging.");
@@ -49,6 +42,19 @@ const NutraLog = () => {
 
   const handleFoodChange = (e) => {
     setFoodItem(e.target.value);
+  };
+
+  const handleDeleteItem = (index) => {
+    const updatedItems = [...loggedItems];
+    updatedItems.splice(index, 1);
+    setLoggedItems(updatedItems);
+  };
+
+  const getTotalCalories = () => {
+    return loggedItems.reduce(
+      (total, item) => total + parseFloat(item.calories),
+      0
+    );
   };
 
   return (
@@ -66,7 +72,7 @@ const NutraLog = () => {
         </label>
         <br />
         <button onClick={logFood}>Log item</button>
-        {calories && <p>Calories: {calories}</p>}
+
         {logError && <p style={{ color: "red" }}>{logError}</p>}
         {loggedItems.length > 0 && (
           <div>
@@ -75,9 +81,13 @@ const NutraLog = () => {
               {loggedItems.map((item, index) => (
                 <li key={index}>
                   {item.foodItem} - Calories: {item.calories}
+                  <button onClick={() => handleDeleteItem(index)}>
+                    Delete
+                  </button>
                 </li>
               ))}
             </ul>
+            <h3>Total calories: {getTotalCalories()}</h3>
           </div>
         )}
       </form>
