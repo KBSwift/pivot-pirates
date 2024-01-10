f0fa002778497f8a679d38a25687e9f8b3d4954b
Connor <c.meriweather55@gmail.com>
2024-01-10 13:58:10 -0600

    added real time weather data using axios/added conditional rendering message to /StrideLog, added weather to nav bar

--- nutra-stride-react/src/layouts/RootLayout.jsx
+++ nutra-stride-react/src/layouts/RootLayout.jsx
@@ -1,10 +1,31 @@
 import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function RootLayout() {
   const location = useLocation();
+  const [weatherData, setWeatherData] = useState(null);
 
   // Check if the current route is /login
   const isLoginPage = location.pathname === "/login";
+
+  const fetchWeatherData = async () => {
+    try {
+      const cityName = "Saint Louis"; // Replace with your desired city name
+      const encodedCityName = encodeURIComponent(cityName);
+
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${encodedCityName}&units=imperial&appid=04efccd56250ed065cecf0b55afcfa3e`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
 
   // Conditionally render the navbar/header
   const renderNavbar = () => {
@@ -17,17 +38,21 @@ export default function RootLayout() {
           <NavLink to="stridelog">StrideLog</NavLink>
           <NavLink to="goals">Goals</NavLink>
           <NavLink to="login">Login</NavLink>
+          {weatherData && (
+            <div className="weather-widget">
+              <p>
+                {weatherData.main.temp}°F, {weatherData.weather[0].description}
+              </p>
+            </div>
+          )}
         </nav>
       );
     }
-    return null;
   };
 
   return (
     <div className="root-layout">
-      <header>
-        {renderNavbar()}
-      </header>
+      <header>{renderNavbar()}</header>
 
       <main>
         <Outlet />
